352ecc676273217cfc4464074480804180aafd0a
u632691 <mhmoscato@teco.com.ar>
2022-11-28 13:55:33 -0300

    Se agrega Suspense en carga de App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import React, { Suspense, useState } from "react";
 import {
   BrowserCompatibility,
   InstantTracker,
   ZapparCamera,
-  ZapparCanvas
+  ZapparCanvas,
+  LogLevel,
+  setLogLevel
 } from "@zappar/zappar-react-three-fiber";
+
 import MainObjects from "./components/MainObjects";
+
+setLogLevel(LogLevel.LOG_LEVEL_VERBOSE);
 
 function App() {
   const [placementMode, setPlacementMode] = useState(true);
+  //const Camera = React.useMemo(()=> <ZapparCamera  />, [])
   return (
     <>
       <BrowserCompatibility />
+      <Suspense fallback={null}>
       <ZapparCanvas>
-        <ZapparCamera />
+      <ZapparCamera  />
         <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
         <MainObjects /> 
         </InstantTracker>
         <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
-
       </ZapparCanvas>
+      </Suspense>
       <div
         id="zappar-button"
         role="button"
